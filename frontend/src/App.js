afc2246d1a079ab85eb4da409e2ce61f0031d918
Arvid Hossain <arvidh97@gmail.com>
2023-06-01 17:36:16 -0400

    splash/signup fully styled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,18 @@ import { Route, Switch } from "react-router-dom";
 import { useSelector } from "react-redux";
 import LoginForm from "./components/LoginForm";
 import SignUpFormPage from "./components/SignUpForm";
+import LoginFormPage from "./components/LoginFormPage";
 
 function App() {
   const sessionUser = useSelector((state) => state.session.user);
   return (
     <div> 
-      <h1>Hello from LinkUp</h1>
       {sessionUser && <h2>Welcome, {sessionUser.fname}!</h2>}
           <Switch>
           <Route exact path='/' component={LoginForm} />
-          <Route exact path='/login' component={LoginForm} />
+          <Route exact path='/login' component={LoginFormPage} />
           <Route exact path="/signup" component={SignUpFormPage} />
           </Switch>
-      <h2> arvid was here</h2>
     </div>
   );
 }
